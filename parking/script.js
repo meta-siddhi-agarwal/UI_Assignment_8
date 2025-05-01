ffc8bb24f30349cc02c63e7ea84557f166141d68
Siddhi Agarwal <siddhi.agarwal@metacube.com>
2025-05-01 22:59:07 +0530

    initial commit

---
+++ parking/script.js
@@ -0,0 +1,29 @@
+// let empName=document.getElementById("name");
+// document.details.form.firstChild.firstChild.addEventListener('keydown',function(event){
+// if(event.code=='Enter'){
+//     alert('sa');
+// }
+// });
+// document.body.childNodes.
+// addEventListener('keydown',function(event){
+
+
+// });
+
+var b=document.querySelector('#name');
+// alert(b);
+b.addEventListener('keydown',function(event){
+    if(event.code=='Enter'){
+        var c=document.querySelectorAll('.gender');
+        
+        
+
+        alert(typeof c);
+        // alert();
+        document.c.visibility=ContentVisibilityAutoStateChangeEvent;
+    }
+    
+});
+// alert(b);
+
+document.querySelector() 
